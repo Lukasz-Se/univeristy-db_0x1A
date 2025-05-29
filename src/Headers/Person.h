3a109c17f8085787e263be76e6fa618be4528a9a
Lukasz-SE <lukasz.semp@gmail.com>
2025-05-29 13:52:21 +0200

    For make polymorphysm possible for Student and Employee classses

---
+++ src/Headers/Person.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "pesel.h"
+
+enum class gender
+{
+    male,
+    female
+};
+
+
+class Person
+{
+public:
+    std::string m_name;
+    std::string m_surname;
+    std::string m_address;
+    pesel m_pesel;
+    gender m_gender;
+};
--- src/Headers/Student.h
+++ src/Headers/Student.h
@@ -1,20 +1,10 @@
 #pragma once
 #include <iostream>
-#include "pesel.h"
+#include "Person.h"
 
-enum class gender
-{
-    male,
-    female
-};
 
-struct Student
-{
-    std::string m_name;
-    std::string m_surname;
-    std::string m_address;
+struct Student : public Person
+{   
     unsigned short int m_indeks_number;
-    pesel m_pesel;
-    gender m_gender;
 };
 
