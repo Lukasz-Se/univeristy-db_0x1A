b8744142fa3d2c8d5bac2f34a0257dcb61b2d049
Lukasz-Se <lukasz.semp@gmail.com>
2025-09-10 17:26:46 +0200

    Delete out directory

--- out/build/x64-debug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Pastor/source/repos/university_db-minproject
-# Build directory: C:/Users/Pastor/source/repos/university_db-minproject/out/build/x64-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("C:/Users/Pastor/source/repos/university_db-minproject/out/build/x64-debug/University_database-ut[1]_include.cmake")
-subdirs("_deps/googletest-build")
--- out/build/x64-debug/_deps/googletest-build/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Pastor/source/repos/university_db-minproject/out/build/x64-debug/_deps/googletest-src/googletest
-# Build directory: C:/Users/Pastor/source/repos/university_db-minproject/out/build/x64-debug/_deps/googletest-build/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
